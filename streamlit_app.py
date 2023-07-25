494f14366ec482c6ce6809c30803e1520ce398f9
Akhil Makol <34644257+akhilmakol@users.noreply.github.com>
2023-07-25 16:37:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-fruit_choice1 = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice1)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice1)
-
 # streamlit.text(fruityvice_response.json())
 
 # Just writes the data to the screen
@@ -58,6 +54,12 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_choice1 = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice1)
+fruityvice_response1= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice1)
+fruityvice_normalized1 = pandas.json_normalize(fruityvice_response1.json())
+streamlit.dataframe(fruityvice_normalized1
 
 
 
