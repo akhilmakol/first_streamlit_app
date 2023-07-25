b4aa30484713d4098bc01196b015c0afef5f87dc
Akhil Makol <34644257+akhilmakol@users.noreply.github.com>
2023-07-25 16:47:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 
 # Github and Streamlit Set-up 
 import streamlit
+import pandas
+import requests
+import snowflake.connector 
+from urlib.error import URLError
+
+
 streamlit.title('My Mom New Healthy Diner')
 
 # Github and Streamlit Iterative Improvements 
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 mega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -13,8 +18,6 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Importing Pandas & Fetching the datafram from AWS S3 Bucket 
-
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -33,8 +36,6 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
-
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -50,7 +51,6 @@ streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
-import snowflake.connector 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
